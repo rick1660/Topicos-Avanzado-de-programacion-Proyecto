b649959cb19c48ffed1d8d1c9d3e821c01137d31
Ricardo Javier <ricardo.jacuinde16@tectijuana.edu.mx>
2020-10-26 15:08:12 -0700

    interfaz

---
+++ Gestor de gastor/Gestor de gastor/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Gestor_de_gastor
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Gestor de gastor/Gestor de gastor/Principal.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gestor_de_gastor
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void BtmPopupSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            BtnOpen.Visibility = Visibility.Collapsed;
+            BtnClose.Visibility = Visibility.Visible;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            BtnOpen.Visibility = Visibility.Visible;
+            BtnClose.Visibility = Visibility.Collapsed;
+        }
+
+       
+    }
+}
